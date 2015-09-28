588484a8e206fb0dba8162db4fc82fba7fdd3d6a
Jennifer Petersen <jenniferpetersen@users.noreply.github.com>
2015-09-28 09:56:47 +0200

    Create Par.java

---
+++ Par.java
@@ -0,0 +1,19 @@
+
+public class Par {
+	private int[] arr;
+	private long invLong;
+	
+	public Par (int[] arr, long invLong) {
+		this.arr = arr;
+		this.invLong = invLong;
+	}
+
+	public int[] getArr() {
+		return arr;
+	}
+	
+	public long getInvLong() {
+		return invLong;
+	}
+
+}
